135eefd5169b09bc85b01c7dacdcb02fb0546da4
Joey <jsparidaans@gmail.com>
2020-12-21 19:23:54 +0100

    added db

---
+++ app/src/main/java/org/example/joeysparidaans/mijntop2000/database/SongDatabase.kt
@@ -0,0 +1,38 @@
+package org.example.joeysparidaans.mijntop2000.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [
+        Song::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+abstract class SongDatabase : RoomDatabase() {
+    abstract val songDatabaseDao: SongDatabaseDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: SongDatabase? = null
+
+        fun getInstance(context: Context): SongDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SongDatabase::class.java,
+                        "song_database"
+                    ).fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
